6417069fa508b4811428d128021659d57883e77e
DenizBasgoren <denizbasgoren1997@gmail.com>
2020-01-19 16:27:49 +0300

    v0.2

--- server.js
+++ server.js
@@ -9,11 +9,17 @@ let staticServer = require('serve-static')(`${__dirname}/dist`, {
     fallthrough: false
 })
 
+const INITIALSCORE = 2000
+const BOTSPEED = 3
+const HUMANSPEED = 3
+const SPAWNAREA = 1000
+const TOTALBOTS = 100
+
 repl.start({ useGlobal: true})
 
 ///   db
 let db = global.db = {
-    players: randomBots(100),
+    players: randomBots(TOTALBOTS),
     rankings: [], // {name: 'Deniz Basgoren', score: 1}
     websockets: new Set()
 }
@@ -29,61 +35,71 @@ function randomBots(n) {
             name: '',
             type: 'bot', // bot human
             status: 'playing', // playing resting
-            score: 10,
+            name: randomName(),
+            score: Math.floor( (Math.random() + 1) ** 13) ,
             color: ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple'][Math.floor(Math.random() * 7)],
             uid: Math.floor(Math.random() * 10**15 )
         }
 
-        if (bots.length === 0) {
-            bot.x = 0,
-            bot.y = 0
-        }
-        else if (bots.length === 1) {
-            bot.x = 100,
-            bot.y = 100
-        }
-        else {
-            let mostDistantX = bots[0].x
-            let mostDistantY = bots[0].y
+        let k = 1
 
+        while ( !bot.x || !bot.y ) {
+            let x = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+            let y = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+
+            let collision = false
             for (let i = 0; i<bots.length; i++) {
-                if (Math.abs(bots[i].x) > mostDistantX ) {
-                    mostDistantX = Math.abs(bots[i].x)
-                }
-                if (Math.abs(bots[i].y) > mostDistantY ) {
-                    mostDistantY = Math.abs(bots[i].y)
+                if (distance({x,y}, bots[i]) < Math.sqrt( .3183 * bot.score) + Math.sqrt( .3183 * bots[i].score) + 10) {
+                    collision = true
+                    break
                 }
             }
 
-            let collision
-            do {
-                collision = false
-                bot.x = Math.random() * (2*mostDistantX+1600) - (mostDistantX+800)
-                bot.y = Math.random() * (2*mostDistantY+1600) - (mostDistantY+800)
+            if (!collision) {
+                bot.x = x
+                bot.y = y
+            }
+            k++
+        }
 
-                // console.log(`len ${bots.length} x ${bot.x.toFixed(2)} y ${bot.y.toFixed(2)} `)
-                for (let i = 0; i<bots.length; i++) {
-                    if ( distance(bot, bots[i]) < bot.score + bots[i].score + 20) {
-                        collision = true
-                        break
-                    }
-                }
+        // if (bots.length === 0) {
+        //     bot.x = 0,
+        //     bot.y = 0
+        // }
+        // else if (bots.length === 1) {
+        //     bot.x = 100,
+        //     bot.y = 100
+        // }
+        // else {
+        //     let mostDistantX = bots[0].x
+        //     let mostDistantY = bots[0].y
 
-            } while ( collision )
+        //     for (let i = 0; i<bots.length; i++) {
+        //         if (Math.abs(bots[i].x) > mostDistantX ) {
+        //             mostDistantX = Math.abs(bots[i].x)
+        //         }
+        //         if (Math.abs(bots[i].y) > mostDistantY ) {
+        //             mostDistantY = Math.abs(bots[i].y)
+        //         }
+        //     }
+
+        //     let collision
+        //     do {
+        //         collision = false
+        //         bot.x = Math.random() * (2*mostDistantX+1600) - (mostDistantX+80000)
+        //         bot.y = Math.random() * (2*mostDistantY+160000) - (mostDistantY+80000)
+
+        //         // console.log(`len ${bots.length} x ${bot.x.toFixed(2)} y ${bot.y.toFixed(2)} `)
+        //         for (let i = 0; i<bots.length; i++) {
+        //             if ( distance(bot, bots[i]) < bot.score + bots[i].score + 20) {
+        //                 collision = true
+        //                 break
+        //             }
+        //         }
+
+        //     } while ( collision )
             
-        }
-
-        let firstLetter = ['B', 'Ch', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'S', 'Sh', 'T', 'W', 'X', 'Y', 'Zh']
-        let secondLetter = ['a', 'e', 'i', 'o', 'u']
-        let thirdLetter = ['', '', '', '', '', 'a', 'e', 'i', 'o', 'u']
-        let fourthLetter = ['', 'ng']
-
-        for (let i = 0; i<2; i++) {
-            bot.name += firstLetter[ Math.floor(Math.random() * firstLetter.length) ]
-            bot.name += secondLetter[ Math.floor(Math.random() * secondLetter.length) ]
-            bot.name += thirdLetter[ Math.floor(Math.random() * thirdLetter.length) ]
-            bot.name += fourthLetter[ Math.floor(Math.random() * fourthLetter.length) ]
-        }
+        // }
 
         bots.push(bot)
     }
@@ -103,6 +119,64 @@ function randomMove() {
     }
 }
 
+function botMove(botIndex) {
+    if (db.players.length === 1) return randomMove()
+
+    let closestEnemyIndex = 0
+    let closestEnemyDistance = distance( db.players[0], db.players[botIndex])
+    let closestTargetIndex = 0
+    let closestTargetDistance = closestEnemyDistance
+
+    for (let i = 0; i< db.players.length; i++) {
+        if (i === botIndex) continue
+
+        let dist = distance( db.players[i], db.players[botIndex])
+        // enemy
+        if (db.players[i].score > db.players[i].score) {
+            if ( dist < closestEnemyDistance) {
+                closestEnemyIndex = i
+                closestEnemyDistance = dist
+            }
+        }
+        else {
+            if ( dist < closestTargetDistance) {
+                closestTargetIndex = i
+                closestTargetDistance = dist
+            }
+        }
+    }
+
+    // gonna attack
+    if (closestEnemyDistance > closestTargetDistance) {
+        return {
+            x: db.players[closestTargetIndex].x - db.players[botIndex].x > 0 ? 1 : -1,
+            y: db.players[closestTargetIndex].y - db.players[botIndex].y > 0 ? 1 : -1,
+        }
+    }
+    else {
+        return {
+            x: db.players[closestTargetIndex].x - db.players[botIndex].x > 0 ? -1 : 1,
+            y: db.players[closestTargetIndex].y - db.players[botIndex].y > 0 ? -1 : 1,
+        }
+    }
+}
+
+function randomName() {
+    let name = ''
+
+    let firstLetter = ['B', 'Ch', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'S', 'Sh', 'T', 'W', 'X', 'Y', 'Zh']
+    let secondLetter = ['a', 'e', 'i', 'o', 'u']
+    let thirdLetter = ['', '', '', '', '', 'a', 'e', 'i', 'o', 'u']
+    let fourthLetter = ['', 'ng']
+
+    for (let i = 0; i<2; i++) {
+        name += firstLetter[ Math.floor(Math.random() * firstLetter.length) ]
+        name += secondLetter[ Math.floor(Math.random() * secondLetter.length) ]
+        name += thirdLetter[ Math.floor(Math.random() * thirdLetter.length) ]
+        name += fourthLetter[ Math.floor(Math.random() * fourthLetter.length) ]
+    }
+    return name
+}
 
 
 
@@ -141,41 +215,64 @@ wsServer.on('connection', socket => {
             let filterName = str => str.substr(0, 10).split('').map(l => l.charCodeAt() >= 32 && l.charCodeAt() <= 126 ? l : '?').join('')
 
             let player = {
-                name: filterName( msg[1] ),
+                name: filterName( msg[1] ) || randomName(),
                 color: msg[2],
-                score: 200,
+                score: INITIALSCORE,
                 type: 'human',
                 status: 'playing', // playing resting
                 uid: Math.floor(Math.random() * 10**15 )
             }
 
-            let mostDistantX = db.players[0].x
-            let mostDistantY = db.players[0].y
+            // let mostDistantX = db.players[0].x
+            // let mostDistantY = db.players[0].y
 
-            for (let i = 0; i<db.players.length; i++) {
-                if (Math.abs(db.players[i].x) > mostDistantX ) {
-                    mostDistantX = Math.abs(db.players[i].x)
-                }
-                if (Math.abs(db.players[i].y) > mostDistantY ) {
-                    mostDistantY = Math.abs(db.players[i].y)
-                }
-            }
+            // for (let i = 0; i<db.players.length; i++) {
+            //     if (Math.abs(db.players[i].x) > mostDistantX ) {
+            //         mostDistantX = Math.abs(db.players[i].x)
+            //     }
+            //     if (Math.abs(db.players[i].y) > mostDistantY ) {
+            //         mostDistantY = Math.abs(db.players[i].y)
+            //     }
+            // }
 
-            let collision
-            do {
-                collision = false
-                player.x = Math.random() * (2*mostDistantX+80) - (mostDistantX+40)
-                player.y = Math.random() * (2*mostDistantY+80) - (mostDistantY+40)
+            // let collision
+            // do {
+            //     collision = false
+            //     player.x = Math.random() * (2*mostDistantX+80) - (mostDistantX+40)
+            //     player.y = Math.random() * (2*mostDistantY+80) - (mostDistantY+40)
 
+            //     for (let i = 0; i<db.players.length; i++) {
+            //         if ( distance(player, db.players[i]) < player.score + db.players[i].score + 20) {
+            //             collision = true
+            //             break
+            //         }
+            //     }
+
+            // } while ( collision )
+            
+
+            let k = 1
+
+            while ( !player.x || !player.y ) {
+                let x = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+                let y = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+
+                let collision = false
                 for (let i = 0; i<db.players.length; i++) {
-                    if ( distance(player, db.players[i]) < player.score + db.players[i].score + 20) {
+                    if (distance({x,y}, db.players[i]) < Math.sqrt( .3183 * player.score) + Math.sqrt( .3183 * db.players[i].score) + 10) {
                         collision = true
                         break
                     }
                 }
 
-            } while ( collision )
-            
+                if (!collision) {
+                    player.x = x
+                    player.y = y
+                }
+                k++
+            }
+
+
             db.players.push(player)
             socket.uid = player.uid
             updateRanking(player)
@@ -192,11 +289,51 @@ wsServer.on('connection', socket => {
             for (let i = 0; i<db.players.length; i++) {
                 if (db.players[i].uid === socket.uid) {
                     // console.log('found')
-                    db.players[i].x += msg[1] *10
-                    db.players[i].y += msg[2] *10
+
+                    db.players[i].x += msg[1] * HUMANSPEED * (msg[1] && msg[2] ? .7 : 1)
+                    db.players[i].y += msg[2] * HUMANSPEED * (msg[1] && msg[2] ? .7 : 1)
                     break
                 }
             }
+        }
+
+        else if (msg[0] === 'restart') {
+
+            let player
+            for (let i = 0; i<db.players.length; i++) {
+                if (db.players[i].uid === socket.uid) {
+                    player = db.players[i]
+                    break
+                }
+            }
+
+            if (!player) return
+            player.x = undefined
+            player.y = undefined
+
+            // new coords
+            let k = 1
+
+            while ( !player.x || !player.y ) {
+                let x = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+                let y = Math.random() * k * 2 * SPAWNAREA - k * SPAWNAREA
+
+                let collision = false
+                for (let i = 0; i<db.players.length; i++) {
+                    if (distance({x,y}, db.players[i]) < Math.sqrt( .3183 * player.score) + Math.sqrt( .3183 * db.players[i].score) + 10) {
+                        collision = true
+                        break
+                    }
+                }
+
+                if (!collision) {
+                    player.x = x
+                    player.y = y
+                }
+                k++
+            }
+            // status playing
+            player.status = 'playing'
         }
         
     })
@@ -255,16 +392,19 @@ setInterval( function serverLoop () {
     
     if (db.websockets.size === 0) return
 
+    // movements
     for (let i = 0; i< db.players.length; i++) {
         if (db.players[i].type === 'bot') {
-            let move = randomMove()
+            // let move = randomMove()
+            let move = botMove( i )
 
             // register move
-            db.players[i].x += move.x *2
-            db.players[i].y += move.y *2
+            db.players[i].x += move.x * BOTSPEED * ( move.x && move.y ? .7 : 1)
+            db.players[i].y += move.y * BOTSPEED * ( move.x && move.y ? .7 : 1)
         }
     }
 
+    //send updates
     db.websockets.forEach(socket => {
         socket.send(JSON.stringify([
             'update',
@@ -275,4 +415,62 @@ setInterval( function serverLoop () {
         ]))
     })
 
-}, 1000/20)
+    // collision detection
+    for (let i = 1; i< db.players.length; i++) {
+
+        if (db.players[i].status === 'resting') continue
+
+        for (let j = 0; j< i; j++) {
+
+            if (db.players[j].status === 'resting') continue
+
+            if ( distance( db.players[i], db.players[j]) < Math.sqrt( .3183 * db.players[i].score) + Math.sqrt( .3183 * db.players[j].score)) {
+
+                // collision detected
+                // let big, small
+                let bigIndex, smallIndex
+                if (db.players[i].score > db.players[j].score ) {
+                    bigIndex = i
+                    smallIndex = j
+                    // big = db.players[bigIndex]
+                    // small = db.players[smallIndex]
+                }
+                else {
+                    bigIndex = j
+                    smallIndex = i
+                    // big = db.players[bigIndex]
+                    // small = db.players[smallIndex]
+                }
+
+                db.players[bigIndex].score += db.players[smallIndex].score
+
+                // debug
+                if (db.players[bigIndex].name === 'k') {
+                    // console.log(`k : ${db.players[bigIndex].score}`)
+                }
+
+                updateRanking( db.players[smallIndex] )
+
+                if (db.players[smallIndex].type === 'bot') {
+                    // new bot
+                    db.players[smallIndex] = randomBots(1)[0]
+                }
+                else {
+                    db.players[smallIndex].status = 'resting'
+                    db.players[smallIndex].score = INITIALSCORE
+                    /////////// inform user that they lost
+
+                    db.websockets.forEach(socket => {
+                        if (socket.uid === db.players[smallIndex].uid) {
+                            socket.send(JSON.stringify([
+                                'gameover'
+                            ]))
+                        }
+                    })
+                    
+                }
+            }
+        }
+    }
+
+}, 1000/3)
